73e7d9c5995baa81b2db8c594a2fe3d353e2b9e8
lumenle <992169498@qq.com>
2021-02-05 17:35:34 +0800

    add sx1278 lora init

--- Display_dhcp_IP/mainwindow.cpp
+++ Display_dhcp_IP/mainwindow.cpp
@@ -32,8 +32,8 @@ QString get_ip(const char *ifname)
 
 }
 int gpio_value[8];
-int spi_dev[2];
-const char spi_devicename[2][32] = {"/dev/spidev0.0","/dev/spidev1.0"};
+int spi_fd[2];
+const char spi_dev[2][32] = {"/dev/spidev0.0","/dev/spidev1.0"};
 uint8_t mode = 0;
 uint8_t bits = 8;
 uint32_t speed = 500000;
@@ -188,110 +188,228 @@ void init_gpio_switch()
     write(gpio_value[8 - 1], SYSFS_GPIO_RST_VAL_H, sizeof(SYSFS_GPIO_RST_VAL_H));
 
 }
-void init_sx1278_spi()
+
+int init_sx1278_spi(int spi_fd, char * spi_dev)
 {
     int ret = 0;
-    spi_dev[0] = open(spi_devicename[0], O_RDWR);
-    if (spi_dev[0] < 0)
+    spi_fd = open(spi_dev, O_RDWR);
+    if (spi_fd < 0)
     {
-
+        return -1;
     }
     /*
      * spi mode
      */
-    ret = ioctl(spi_dev[0], SPI_IOC_WR_MODE, &mode);
+    ret = ioctl(spi_fd, SPI_IOC_WR_MODE, &mode);
     if (ret == -1)
     {
-
+        return -2;
     }
 
-    ret = ioctl(spi_dev[0], SPI_IOC_RD_MODE, &mode);
+    ret = ioctl(spi_fd, SPI_IOC_RD_MODE, &mode);
     if (ret == -1)
     {
-
-    }
-    /*
-     * bits per word
-     */
-    ret = ioctl(spi_dev[0], SPI_IOC_WR_BITS_PER_WORD, &bits);
-    if (ret == -1)
-    {
-
-    }
-
-    ret = ioctl(spi_dev[0], SPI_IOC_RD_BITS_PER_WORD, &bits);
-    if (ret == -1)
-    {
-
-    }
-    /*
-     * max speed hz
-     */
-    ret = ioctl(spi_dev[0], SPI_IOC_WR_MAX_SPEED_HZ, &speed);
-    if (ret == -1)
-    {
-
-    }
-
-    ret = ioctl(spi_dev[0], SPI_IOC_RD_MAX_SPEED_HZ, &speed);
-    if (ret == -1)
-    {
-
-    }
-
-
-
-
-    spi_dev[1] = open(spi_devicename[1], O_RDWR);
-    if (spi_dev[0] < 0)
-    {
-
-    }
-    /*
-     * spi mode
-     */
-    ret = ioctl(spi_dev[1], SPI_IOC_WR_MODE, &mode);
-    if (ret == -1)
-    {
-
-    }
-
-    ret = ioctl(spi_dev[1], SPI_IOC_RD_MODE, &mode);
-    if (ret == -1)
-    {
-
+        return -2;
     }
     /*
      * bits per word
      */
-    ret = ioctl(spi_dev[1], SPI_IOC_WR_BITS_PER_WORD, &bits);
+    ret = ioctl(spi_fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
     if (ret == -1)
     {
-
+         return -2;
     }
 
-    ret = ioctl(spi_dev[1], SPI_IOC_RD_BITS_PER_WORD, &bits);
+    ret = ioctl(spi_fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
     if (ret == -1)
     {
-
+        return -2;
     }
     /*
      * max speed hz
      */
-    ret = ioctl(spi_dev[1], SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+    ret = ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
     if (ret == -1)
     {
-
+        return -2;
     }
 
-    ret = ioctl(spi_dev[1], SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+    ret = ioctl(spi_fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
     if (ret == -1)
     {
-
+        return -2;
     }
     std::cout<<"spi mode: "<<mode<<std::endl;
     std::cout<<"bits per word: "<<bits<<std::endl;
     std::cout<<"max speed: "<<speed<<"Hz"<<std::endl;
+
+}
+int spi_transfer(int spi_fd,uint8_t tx[],uint8_t rx[],uint16_t len)
+{
+    int ret;
+    struct spi_ioc_transfer tr;  /* 这种写法一定要赋初值 */
+    memset(&tr,0,sizeof(struct spi_ioc_transfer));
+    tr.tx_buf = (unsigned long)tx;
+    tr.rx_buf = (unsigned long)rx;
+    tr.len = len;
+    tr.delay_usecs = delay;
+    tr.speed_hz = speed;
+    tr.bits_per_word = bits;
+    ret = ioctl(spi_fd, SPI_IOC_MESSAGE(1), &tr);
+    if (ret < 1)
+    {
+        ret = -1;
+        return ret;
+    }
+    return ret;
+}
+int sx1278_set_SleepMode(int spi_fd)
+{
+    int ret;
+    uint8_t tx[] = {
+        0x01|0x80, 0x08 /*sx1278 set sleep mode*/
+    };
+    uint8_t rx[ARRAY_SIZE(tx)] = {0, };
+    ret = spi_transfer(spi_fd,tx,rx,ARRAY_SIZE(tx));
+    if (ret < 1)
+    {
+        ret = -1;
+        return ret;
+    }
+    return ret;
+}
+int sx1278_EntryLora(int spi_fd)
+{
+    int ret;
+    uint8_t tx[] = {
+        0x01|0x80, 0x88 /*sx1278 entry lora*/
+    };
+    uint8_t rx[ARRAY_SIZE(tx)] = {0, };
+    ret = spi_transfer(spi_fd,tx,rx,ARRAY_SIZE(tx));
+    if (ret < 1)
+    {
+        ret = -1;
+        return ret;
+    }
+    return ret;
+}
+int sx1278_set_frequency(int spi_fd)
+{
+    int ret;
+    uint8_t tx[] = {
+        0x06|0x80, 0x85,/*434MHz @ 26m*/
+        0x07|0x80, 0x3b,
+        0x08|0x80, 0x13
+    };
+    uint8_t rx[ARRAY_SIZE(tx)] = {0, };
+    ret = spi_transfer(spi_fd,tx,rx,ARRAY_SIZE(tx));
+    if (ret < 1)
+    {
+        ret = -1;
+        return ret;
+    }
+    return ret;
+}
+int sx1278_set_outpowerparameter(int spi_fd)
+{
+    int ret;
+    uint8_t tx[] = {
+        0x09|0x80,0xFF                   //20dbm
+    };
+    uint8_t rx[ARRAY_SIZE(tx)] = {0, };
+    ret = spi_transfer(spi_fd,tx,rx,ARRAY_SIZE(tx));
+    if (ret < 1)
+    {
+        ret = -1;
+        return ret;
+    }
+    return ret;
+}
+int sx1278_set_SpreadFactor(int spi_fd)
+{
+    int ret;
+    uint8_t tx[] = {
+        0x1d|0x80,0x68,    //Explicit Enable CRC Enable(0x02) & Error Coding rate 4/5(0x01), 4/6(0x02), 4/7(0x03), 4/8(0x04)
+        0x1e|0x80,0x97,    //SFactor &  LNA gain set by the internal AGC loop
+        0x26|0x80,0x08,     //LowDataRateOptimize en
+        0x1f|0x80,0xff,    //RegSymbTimeoutLsb Timeout = 0x3FF(Max)
+        0x20|0x80,0x00,     //RegPreambleMsb
+        0x21|0x80,0x10,     //RegPreambleLsb 8+4=12byte Preamble  16+4=20
+        0x41|0x80,0x01
+    };
+    uint8_t rx[ARRAY_SIZE(tx)] = {0, };
+    ret = spi_transfer(spi_fd,tx,rx,ARRAY_SIZE(tx));
+    if (ret < 1)
+    {
+        ret = -1;
+        return ret;
+    }
+    return ret;
+}
+int sx1278_set_Standby(int spi_fd)
+{
+    int ret;
+    uint8_t tx[] = {
+        0x01|0x80,0x09,     //Entry standby mode
+        0x4d|0x80,0x87,     //Tx for 20dBm
+        0x24|0x80,0xff      //RegHopPeriod NO FHSS
+    };
+    uint8_t rx[ARRAY_SIZE(tx)] = {0, };
+    ret = spi_transfer(spi_fd,tx,rx,ARRAY_SIZE(tx));
+    if (ret < 1)
+    {
+        ret = -1;
+        return ret;
+    }
+    return ret;
+}
+int sx1278_init_lora(int spi_fd)
+{
+    int ret = sx1278_set_SleepMode(spi_fd);
+    if (ret < 1)
+    {
+        ret = -1;
+        return ret;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    ret = sx1278_EntryLora(spi_fd);
+    if (ret < 1)
+    {
+        ret = -2;
+        return ret;
+    }
+    ret = sx1278_set_frequency(spi_fd);
+    if (ret < 1)
+    {
+        ret = -3;
+        return ret;
+    }
+    ret = sx1278_set_outpowerparameter(spi_fd);
+    if (ret < 1)
+    {
+        ret = -4;
+        return ret;
+    }
+    ret = sx1278_set_SpreadFactor(spi_fd);
+    if (ret < 1)
+    {
+        ret = -5;
+        return ret;
+    }
+    ret = sx1278_set_Standby(spi_fd);
+    if (ret < 1)
+    {
+        ret = -6;
+        return ret;
+    }
+
+}
+std::string sx1278_lora_rx(int spi_fd)
+{
+
+}
+int sx1278_lora_tx(std::string &txbuf,int spi_fd)
+{
 
 }
 std::string get_sx1278_chipid(int spi_fd)
@@ -301,19 +419,20 @@ std::string get_sx1278_chipid(int spi_fd)
         0x42&0x7f, 0xFF
     };
     uint8_t rx[ARRAY_SIZE(tx)] = {0, };
-    struct spi_ioc_transfer tr;  /* 这种写法一定要赋初值 */
-    memset(&tr,0,sizeof(struct spi_ioc_transfer));
-    tr.tx_buf = (unsigned long)tx;
-    tr.rx_buf = (unsigned long)rx;
-    tr.len = ARRAY_SIZE(tx);
-    tr.delay_usecs = delay;
-    tr.speed_hz = speed;
-    tr.bits_per_word = bits;
-    ret = ioctl(spi_fd, SPI_IOC_MESSAGE(1), &tr);
-    if (ret < 1)
-    {
+//    struct spi_ioc_transfer tr;  /* 这种写法一定要赋初值 */
+//    memset(&tr,0,sizeof(struct spi_ioc_transfer));
+//    tr.tx_buf = (unsigned long)tx;
+//    tr.rx_buf = (unsigned long)rx;
+//    tr.len = ARRAY_SIZE(tx);
+//    tr.delay_usecs = delay;
+//    tr.speed_hz = speed;
+//    tr.bits_per_word = bits;
+//    ret = ioctl(spi_fd, SPI_IOC_MESSAGE(1), &tr);
+//    if (ret < 1)
+//    {
 
-    }
+//    }
+    spi_transfer(spi_fd,tx,rx,ARRAY_SIZE(tx));
     char chipid_str[16];
     snprintf(chipid_str,4,"%.2X",rx[1]);
     std::string chipid(chipid_str);
@@ -331,11 +450,22 @@ MainWindow::MainWindow(QWidget *parent,QApplication *a) :
     ui->setupUi(this);
     qDebug() << QSqlDatabase::drivers();//打印qt支持的数据库类型
     init_gpio_switch();
-    init_sx1278_spi();
-    std::string chipid = get_sx1278_chipid(spi_dev[0]);
+    init_sx1278_spi(spi_fd[0], (char *)spi_dev[0]);
+    init_sx1278_spi(spi_fd[1], (char *)spi_dev[1]);
+    std::string chipid = get_sx1278_chipid(spi_fd[0]);
     std::cout<<"spi_dev0 chipid: "<<chipid<<std::endl;
-    chipid = get_sx1278_chipid(spi_dev[1]);
+    chipid = get_sx1278_chipid(spi_fd[1]);
     std::cout<<"spi_dev1 chipid: "<<chipid<<std::endl;
+    int spi_init_ret = sx1278_init_lora(spi_fd[0]);
+    if(spi_init_ret < 1)
+    {
+        std::cout<<"spi_dev0 init sx1278 lora failed ret: "<<spi_init_ret<<std::endl;
+    }
+    spi_init_ret = sx1278_init_lora(spi_fd[1]);
+    if(spi_init_ret < 1)
+    {
+        std::cout<<"spi_dev1 init sx1278 lora failed ret: "<<spi_init_ret<<std::endl;
+    }
     db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("rm-2vcyj9v8ozxj7b2m4to.mysql.cn-chengdu.rds.aliyuncs.com");
     db.setDatabaseName("ejcdb");
